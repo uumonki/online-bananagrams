f3bd334260bffdcac4b09d22d459722ad49e1af3
uumonki <48245999+uumonki@users.noreply.github.com>
2025-04-03 17:31:38 +0900

    naming folder ws is messing shit up

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
-import { setupWebSocket } from 'ws';
+import { setupWebSocket } from 'websocket';
 
 const app = express();
 const server = createServer(app);
